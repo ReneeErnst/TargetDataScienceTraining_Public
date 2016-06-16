e38084bb9f4e76875b679290cd2a094cd659db3e
ReneeMurray <Renee.Murray@target.com>
2016-06-16 14:34:53 -0500

    Minor updates to code formatting
    Making the code easier to read (for me, at least).

--- Notebooks/Kmeans.py
+++ Notebooks/Kmeans.py
@@ -28,46 +28,51 @@ def stop_criterion(centroid_points_old, centroid_points_new,T):
 
 class MRKmeans(MRJob):
     centroid_points=[]
-    k=3    
+    k = 3    
     def steps(self):
         return [
-            MRStep(mapper_init = self.mapper_init, mapper=self.mapper,combiner = self.combiner,reducer=self.reducer)
-               ]
+            MRStep(mapper_init = self.mapper_init, 
+            mapper=self.mapper,
+            combiner = self.combiner,
+            reducer=self.reducer)
+       ]
     #load centroids info from file
     def mapper_init(self):
         print "Current path:", os.path.dirname(os.path.realpath(__file__))
         
-        self.centroid_points = [map(float,s.split('\n')[0].split(',')) for s in open("Centroids.txt").readlines()]
+        self.centroid_points = [map(float, s.split('\n')[0].split(',')) for s in open("Centroids.txt").readlines()]
         #open('Centroids.txt', 'w').close()
         
         print "Centroids: ", self.centroid_points
         
     #load data and output the nearest centroid index and data point 
     def mapper(self, _, line):
-        D = (map(float,line.split(',')))
-        yield int(MinDist(D,self.centroid_points)), (D[0],D[1],1)
+        D = (map(float, line.split(',')))
+        yield int(MinDist(D, self.centroid_points)), (D[0], D[1], 1)
+    
     #Combine sum of data points locally
     def combiner(self, idx, inputdata):
         sumx = sumy = num = 0
-        for x,y,n in inputdata:
+        for x, y, n in inputdata:
             num = num + n
             sumx = sumx + x
             sumy = sumy + y
-        yield idx,(sumx,sumy,num)
+        yield idx,(sumx, sumy, num)
+    
     #Aggregate sum for each cluster and then calculate the new centroids
     def reducer(self, idx, inputdata): 
         centroids = []
-        num = [0]*self.k 
+        num = [0] * self.k 
         for i in range(self.k):
-            centroids.append([0,0])
+            centroids.append([0, 0])
         for x, y, n in inputdata:
             num[idx] = num[idx] + n
             centroids[idx][0] = centroids[idx][0] + x
             centroids[idx][1] = centroids[idx][1] + y
-        centroids[idx][0] = centroids[idx][0]/num[idx]
-        centroids[idx][1] = centroids[idx][1]/num[idx]
+        centroids[idx][0] = centroids[idx][0] / num[idx]
+        centroids[idx][1] = centroids[idx][1] / num[idx]
 
-        yield idx,(centroids[idx][0],centroids[idx][1])
-      
+        yield idx, (centroids[idx][0], centroids[idx][1])
+
 if __name__ == '__main__':
     MRKmeans.run()
