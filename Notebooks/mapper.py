10f504ddc8250ca39c7f7c1ce22d3880df47911a
ReneeMurray <Renee.Murray@target.com>
2016-06-23 10:25:49 -0500

    Updated files

---
+++ Notebooks/mapper.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import sys, re, string
+# define regex for punctuation removal
+regex = re.compile('[%s]' % re.escape(string.punctuation))
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # use subject and body
+    line = line.strip().split('\t', 2)[-1]
+    # remove punctuations, only have white-space as delimiter
+    line = regex.sub(' ', line.lower())
+    line = re.sub( '\s+', ' ', line )
+    # split the line into words
+    words = line.split()
+    # increase counters
+    for word in words:
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+        if len(word) > 1:  #drop single character words
+            print '%s\t%s' % (word, 1)
